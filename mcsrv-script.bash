2cc312c9d97eb5858c04d2737060d4919a3acb70
7thCore <34578834+7thCore@users.noreply.github.com>
2019-09-10 16:09:23 +0200

    Update mcsrv-script.bash

--- mcsrv-script.bash
+++ mcsrv-script.bash
@@ -3,7 +3,7 @@
 #Interstellar Rift server script by 7thCore
 #If you do not know what any of these settings are you are better off leaving them alone. One thing might brake the other if you fiddle around with it.
 #Leave this variable alone, it is tied in with the systemd service file so it changes accordingly by it.
-export VERSION="201908211050"
+export VERSION="201909101610"
 
 #Basics
 export NAME="McSrv" #Name of the screen
